c64e9d2dd59a0a0dee85e02e14934576c7fc4e87
canziani14 <mcanziani12@gmail.com>
2022-06-02 21:08:14 -0300

    agrego proyecto

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath = path.resolve( __dirname, './public' );
+
+
+app.listen(3010,() => {
+    console.log("Servidor corriendo!");
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/index', (req,res)=>{
+    res.sendFile(path.join( __dirname,'./views/index.html'))
+})
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join( __dirname,'./views/login.html'));
+  });
+  app.get('/signin', function (req, res) {
+    res.sendFile(path.join( __dirname,'./views/signin.html'));
+  });
+  app.get('/edit-account', function (req, res) {
+    res.sendFile(path.join( __dirname,'./views/edit-account.html'));
+  });
+  app.get('/change-password', function (req, res) {
+    res.sendFile(path.join( __dirname,'/views/change-password.html'));
+  });
