71bb3072a9c5ad06597c6dfeb09275c5f6c04118
Javi <javier.amarilla@casademoneda.gob.ar>
2022-06-21 19:15:23 -0300

    cambio en lab.ejs

--- controller/membershipsController.js
+++ controller/membershipsController.js
@@ -1,11 +1,35 @@
 const path = require("path");
-
+const memberships = [
+  {
+    id: 1,
+    nombre: "Lab",
+    precio: 150,
+  },
+  {
+    id:2,
+    nombre: "Flex",
+    precio: 300
+  },
+  {
+    id:3,
+    nombre: "Office",
+    precio: 750
+  },
+  {
+    id:4,
+    nombre: "Desk",
+    precio: 450
+  }
+];
+const aa= "aaa"
 const membershipsController = {
   home: function (req, res) {
     res.render("memberships");
   },
   lab: function (req, res) {
-    res.render("lab");
+    res.render("lab",{
+      membership: memberships[0]
+    });
   },
   flex: function (req, res) {
     res.render("flex");
