976e188978d5843a08ff24ababc099ce25ff7d80
Javi <javier.amarilla@casademoneda.gob.ar>
2022-07-20 11:07:32 -0300

    agrego iconos en admin

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -2,13 +2,32 @@ const path = require("path");
 const fs = require("fs");
 
 
-let memberships = JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','database','memberships.json')))
-
-
 module.exports = {
     index: (req, res) =>{
-      res.render (path.join(__dirname, "../views/admin/index"),{
+      let memberships = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/memberships.json')));
+      
+      res.render (path.join(__dirname, "../views/admin/admin"),{
         memberships: memberships
     })
-    }
+    },
+    create: (req,res)=>{
+      res.render(path.resolve(__dirname, '../views/admin/create'));
+    },
+    save: (req,res)=>{
+      let memberships = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/memberships.json')));
+      let ultimaMembership = memberships.pop();
+      memberships.push(ultimaMembership);
+      let nuevaMembership={
+        id: ultimaMembership +1,
+        name: req.body.name,
+        services: req.body.services,
+        details: req.body.details,
+        price: req.body.price,
+        img: req.body.img,
+      }
+      memberships.push(nuevaMembership);
+      let nuevaMembershipAGuardar = JSON.stringify(memberships, null, 2);
+      fs.writeFileSync(path.resolve(__dirname,'../database/memberships.json'), nuevaMembershipAGuardar);
+        res.redirect('/admin');
+    }, 
 };
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const adminController = require('../controller/adminController');
+const path = require('path');
+const multer = require('multer');
+const adminController = require("../controller/adminController");
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '../../public/images/motos'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'moto-'+Date.now()+path.extname(file.originalname))
+    }
+  })
+   
+  const upload = multer({ storage })
 
-router.get('/admin', adminController.index);
+router.get("/admin", adminController.index);
+router.get('/admin/create',adminController.create);
+router.post('admin/create',upload.single('imagen'),adminController.save)
+// router.get('/administrar/detail/:id', controllersAdmin.show);
+// router.get('/administrar/edit/:id', controllersAdmin.edit);
+
 
 module.exports = router;
