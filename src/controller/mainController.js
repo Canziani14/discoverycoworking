0e1d8970f28ee3d3f2d5b9c6340341a3c7b313f1
canziani14 <mcanziani12@gmail.com>
2022-08-02 22:25:44 -0300

    membership responsive

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -2,11 +2,11 @@ const path = require("path");
 const fs = require("fs");
 
 
-const membershipsFilePath = path.join(__dirname,'../database/memberships.json');
+const membershipsFilePath = path.join(__dirname, '../database/memberships.json');
 const memberships = JSON.parse(fs.readFileSync(membershipsFilePath, "utf-8"));
 
 const mainController = {
-  index:  (req, res) =>{
+  index: (req, res) => {
     if (req.session.userLoged) {
       res.render("index", {
         title: "Inicio",
@@ -15,43 +15,36 @@ const mainController = {
         memberships: memberships
       });
     } else {
-      res.render("index", { title: "Inicio", styles: "index.css",memberships });
+      res.render("index", { title: "Inicio", styles: "index.css", memberships });
     }
   },
   carrito: function (req, res) {
-    
+
     //let idMembership = req.baseUrl
-    
-      let nameMembership = req.params.nameMembership;
-  
-      const filteredMembership = memberships.find((membership)=>{
-        return membership.name == nameMembership;
-      })
-  
-      if (req.session.userLoged) {
-        res.render("carrito", {
-          title: filteredMembership.name,
-          styles: "carrito.css",
-          membership: filteredMembership,
-          user: req.session.userLoged,
-        });
-      } else {
-        res.render("carrito", {
-          title: filteredMembership.title,
-          styles: "carrito.css",
-          membership: filteredMembership,
-        });
-      }
-    },
+
+    let nameMembership = req.params.nameMembership;
+
+    const filteredMembership = memberships.find((membership) => {
+      return membership.name == nameMembership;
+    })
+
+    if (req.session.userLoged) {
+      res.render("carrito", {
+        title: filteredMembership.name,
+        styles: "carrito.css",
+        membership: filteredMembership,
+        user: req.session.userLoged,
+      });
+    } else {
+      res.render("carrito", {
+        title: filteredMembership.title,
+        styles: "carrito.css",
+        membership: filteredMembership,
+      });
     }
-/*
-    res.render("carrito", {
-      title: "Carrito",
-      styles: "carrito.css",
-      user: req.session.userLoged,
-      memberships: memberships
-    } );
-  }*/
+  },
+}
+
 
 
 module.exports = mainController;
