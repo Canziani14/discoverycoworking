44a96f7990035204f93811ac219b7d74430dd38c
Jav1stro <javiamarilla1@gmail.com>
2022-09-02 12:36:25 -0300

    un cachito de css

---
+++ src/js/login.js
@@ -0,0 +1,12 @@
+// let formulario = documents.forms["form"];
+
+// formulario.onsubmit =(event)=>{
+//     event.preventDefault();
+
+//     let errores =[];
+//     let campoEmail = document.getElementById('email');
+
+//     if(campoEmail == "hola@hola.com"){
+//         alert('DALE BOBI')
+//     }
+// }
