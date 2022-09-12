0fad2165c4653ac7889812e9fd174c4991b12c10
canziani14 <mcanziani12@gmail.com>
2022-09-12 13:52:07 -0300

    inicio validaciones en el front

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,63 @@
-// let formulario = documents.forms["form"];
+window.addEventListener('load', function () {
 
-// formulario.onsubmit =(event)=>{
-//     event.preventDefault();
+    //Capturar el formulario 
+    let formulario = document.querySelector('.form');
+    //console.log(formulario.elements.email.value);
+    formulario.addEventListener('submit', function (evento) {
 
-//     let errores =[];
-//     let campoEmail = document.getElementById('email');
+        if (!validaciones(evento)) {
+            evento.preventDefault();
+        } else {
+            formulario.submit();
+        }
 
-//     if(campoEmail == "hola@hola.com"){
-//         alert('DALE BOBI')
-//     }
-// }
+        function validaciones(evento) {
+
+            //Destructuring  
+            let { email, password } = formulario.elements;
+            let errors = [];
+            console.log(formulario.elements.email.value);
+            //Validar el email - Expresiones Regulares https://www.w3schools.com/jsref/jsref_obj_regexp.asp       https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+            let reEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+            if (!reEmail.test(email.value)) {
+                errors.push('El email es inválido...');
+                email.classList.add('is-invalid');
+                //errores['last_name'] = 'El campo nombre no puede estar vacio...';
+            } else {
+                email.classList.add('is-valid');
+                email.classList.remove('is-invalid');
+            }
+            //Aquí valido el password haciendo uso de Expresiones Regulares
+            //Esta expresión regular valida como Mínimo seis caracteres, al menos una letra y un número:
+            let rePassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+            if (!rePassword.test(password.value)) {
+                errors.push('La contraseña como mínimo debe tener seis caracteres, al menos una letra y un número');
+                password.classList.add('is-invalid');
+                //errores['last_name'] = 'El campo nombre no puede estar vacio...';
+            } else {
+                password.classList.add('is-valid');
+                password.classList.remove('is-invalid');
+            }
+
+            //Aquí enviamos los errores al usuario
+            let ulErrores = document.getElementById('errors');
+            ulErrores.classList.add('alert-danger')
+            if (errors.length > 0) {
+
+                evento.preventDefault();
+                ulErrores.innerHTML = "";
+                for (let i = 0; i < errors.length; i++) {
+                    ulErrores.innerHTML += `<li> ${errors[i]} </li> `
+                }
+                errores = [];
+            } else {
+                return true;
+            }
+        }
+
+    })
+
+
+
+})
