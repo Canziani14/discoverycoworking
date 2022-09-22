334b9c005e1f6c4503318175a1e3d277188059f4
canziani14 <mcanziani12@gmail.com>
2022-09-22 19:33:20 -0300

    soluciono errores en login con el front

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
-const db = require("../../src/database/models");
+//const db = require("../../src/database/models");
 
 window.addEventListener('load', function () {
-const ulErrores = document.getElementById('erroresFront');
+    const ulErrores = document.getElementById('erroresFront');
 
     //Capturar el formulario 
     let formulario = document.querySelector('.form');
@@ -19,15 +19,17 @@ const ulErrores = document.getElementById('erroresFront');
             let { email, password } = formulario.elements;
             let errores = [];
             let reEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            
+
+
             if (email.value == '') {
                 errores.push('El campo email no puede estar vacio');
                 email.classList.add('is-invalid');
                 ulErrores.classList.add('showErrores');
             } else {
                 email.classList.add('is-valid');
-                email.classList.remove('is-invalid');}
-            
+                email.classList.remove('is-invalid');
+            }
+
             if (!reEmail.test(email.value)) {
                 errores.push('El email es inválido...');
                 email.classList.add('is-invalid');
@@ -37,23 +39,9 @@ const ulErrores = document.getElementById('erroresFront');
                 email.classList.add('is-valid');
                 email.classList.remove('is-invalid');
             }
-/*
-            db.User.findAll()
-            .then ( function(user) {
-                if (user.email != req.body.email) {
-                    errores.push('El email no esta registrado en la base de datos...');
-                    email.classList.add('is-invalid');
-                    ulErrores.classList.add('showErrores')
-                }  else {
-                    email.classList.add('is-valid');
-                    email.classList.remove('is-invalid');
-                }
-            })
-*/
-            //Aquí valido el password haciendo uso de Expresiones Regulares
-            //Esta expresión regular valida como Mínimo seis caracteres, al menos una letra y un número:
-          // let rePassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
-            if(password.value.length<6){
+
+            let rePassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+            if (password.value.length < 6) {
                 errores.push('La contraseña como mínimo debe tener seis caracteres');
                 password.classList.add('is-invalid');
                 ulErrores.classList.add('showErrores')
@@ -64,13 +52,31 @@ const ulErrores = document.getElementById('erroresFront');
             }
 
             if (password.value == '') {
-                password.push('El campo password no puede estar vacio');
-                email.classList.add('is-invalid');
+                errores.push('El campo password no puede estar vacio');
+                password.classList.add('is-invalid');
                 ulErrores.classList.add('showErrores');
             } else {
                 password.classList.add('is-valid');
-                password.classList.remove('is-invalid');}
+                password.classList.remove('is-invalid');
+            }
 
+          
+
+            /*  
+               
+              
+               db.User.findAll()
+               .then ( function(user) {
+                   if (user.email != req.body.email) {
+                       errores.push('El email no esta registrado en la base de datos...');
+                       email.classList.add('is-invalid');
+                       ulErrores.classList.add('showErrores')
+                   }  else {
+                       email.classList.add('is-valid');
+                       email.classList.remove('is-invalid');
+                   }
+               })
+   */
             //Aquí enviamos los errores al usuario
 
 
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -16,59 +16,61 @@ const userController = {
   },
   processLogin: function (req, res) {
     User.findAll()
-    .then((users) => {		
-      //Aquí guardo los errores que vienen desde la ruta, valiendome del validationResult
-      let errors = validationResult(req);
-      
-      let userLogged = [];
-      
-      if(req.body.email != '' && req.body.password != ''){
+      .then((users) => {
+        //Aquí guardo los errores que vienen desde la ruta, valiendome del validationResult
+        let errors = validationResult(req);
 
-        userLogged = users.filter(function (user) {
-            return user.userEmail === req.body.email  
-          });
-          console.log('USUARIO LOGGEADO',userLogged)
+        let userLogged = [];
 
-        if(userLogged.length > 0){
+        if (req.body.email != '' && req.body.password != '') {
+
           userLogged = users.filter(function (user) {
-              return user.userEmail === req.body.email  
+            return user.userEmail === req.body.email
+          });
+          console.log('USUARIO LOGGEADO', userLogged)
+
+          if (userLogged.length > 0) {
+            userLogged = users.filter(function (user) {
+              return user.userEmail === req.body.email
             });
-            console.log('USUARIO LOGGEADO',userLogged[0].userName)
-  }
-        else{
-          console.log('No hay un email registrado con estos valores.');
-          return res.render(path.resolve(__dirname, '../views/users/login'),{ errors: [{ msg: "Este email no está registrado." }],
-          title: "Login",
-          styles: "login.css",
-        })
+            console.log('USUARIO LOGGEADO', userLogged[0].userName)
+          }
+          else {
+            console.log('No hay un email registrado con estos valores.');
+            return res.render(path.resolve(__dirname, '../views/users/login'), {
+              errors: [{ msg: "Este email no está registrado." }],
+              title: "Login",
+              styles: "login.css",
+            })
+          }
+
+        }
+        if (bcrypt.compareSync(req.body.password, userLogged[0].password) !== true) {
+          console.log(req.body.password)
+          userLogged = [];
         }
 
-      }
-       if(bcrypt.compareSync(req.body.password,userLogged[0].password) !== true){
-    console.log(req.body.password)
-    userLogged = [];
-  }
+        if (userLogged.length === 0) {
+          console.log('No hay usuario loggeado por credenciales invalidas, se vuelve a cargar el login')
+          return res.render(path.resolve(__dirname, '../views/users/login'), {
+            errors: [{ msg: "Credenciales invalidas" }],
+            title: "Login",
+            styles: "login.css",
+          })
+        } else {
+          //Aquí guardo en SESSION al usuario logueado
+          console.log('Se logeo el usuario!')
+          console.log('USUARIO LOGGEADO', userLogged[0].userName)
 
-     if (userLogged.length === 0) {
-        console.log('No hay usuario loggeado por credenciales invalidas, se vuelve a cargar el login')
-        return res.render(path.resolve(__dirname, '../views/users/login'),{ errors: [{ msg: "Credenciales invalidas" }],
-          title: "Login",
-          styles: "login.css",
-        })
-      } else {
-        //Aquí guardo en SESSION al usuario logueado
-        console.log('Se logeo el usuario!')
-        console.log('USUARIO LOGGEADO',userLogged[0].userName)
+          req.session.user = userLogged[0];
+        }
+        //Aquí verifico si el usuario le dio click en el check box para recordar al usuario 
+        if (req.body.recordarme) {
+          res.cookie('email', userLogged[0].email, { maxAge: 1000 * 60 * 60 * 24 })
+        }
+        return res.redirect('/');
 
-        req.session.user = userLogged[0];
-      }
-      //Aquí verifico si el usuario le dio click en el check box para recordar al usuario 
-      if(req.body.recordarme){
-        res.cookie('email',userLogged[0].email,{maxAge: 1000 * 60 * 60 * 24})
-      }
-      return res.redirect('/'); 
-
-  })
+      })
   },
   signin: function (req, res) {
     res.render("users/signin", {
@@ -86,15 +88,15 @@ const userController = {
         styles: "login.css",
       });
     }
-      User.create({
+    User.create({
       userName: req.body.userName,
       lastName: req.body.userLastName,
       userEmail: req.body.userEmail,
-      password: req.body.password =  bcrypt.hashSync(req.body.password, 10),
-      avatar:req.file ? req.file.filename : '',
+      password: req.body.password = bcrypt.hashSync(req.body.password, 10),
+      avatar: req.file ? req.file.filename : '',
     })
-      
-      .then ( function(result) {
+
+      .then(function (result) {
         res.render("users/login", {
           title: "Home",
           styles: "login.css",
@@ -113,72 +115,72 @@ const userController = {
   contactus: function (req, res) {
     res.render("./users/contactus");
   },
-  contact:(req, res) => {
-      db.ContactUs.create({
+  contact: (req, res) => {
+    db.ContactUs.create({
       name: req.body.name,
       email: req.body.email,
       comments: req.body.comments,
     })
-    .then ( function(result) {
-      res.render("users/contactUsSend", {
-        title: "Contact Us",
-        styles: "carrito.css",
-        user: req.session.userLogged,
-      });
-    })
-    .catch(error => console.log(error));
-    
-      
+      .then(function (result) {
+        res.render("users/contactUsSend", {
+          title: "Contact Us",
+          styles: "carrito.css",
+          user: req.session.userLogged,
+        });
+      })
+      .catch(error => console.log(error));
+
+
   },
 
 
   shopp: function (req, res) {
     console.log(req.session.user)
-        res.render("users/shopp", {
-          title: "Carrito",
-          styles: "carrito.css",
-          user: req.session.user,
-          memberships: req.session.user
-        })
+    res.render("users/shopp", {
+      title: "Carrito",
+      styles: "carrito.css",
+      user: req.session.user,
+      memberships: req.session.user
+    })
   },
 
   toBuy: function (req, res) {
-    
-    let id = db.Membership.findByPk (req.params.idMembership)
-    .then (function (newMembership){
-      let userLogin=req.session.user.id_users;
-      console.log("usuario conectado",)
-      
-      User.update(
-        {
-          membership: newMembership.name
-        },
-        {
-          
-          where:{id_users:userLogin}
-        }
-      )
-      
-      res.render("users/toBuy", {
-        title: "successful purchase",
-        styles: "carrito.css",
-        user: req.session.user,
-        newMembership
-      });
-      
-    })     
+
+    let id = db.Membership.findByPk(req.params.idMembership)
+      .then(function (newMembership) {
+        let userLogin = req.session.user.id_users;
+        console.log("usuario conectado",)
+
+        User.update(
+          {
+            membership: newMembership.name
+          },
+          {
+
+            where: { id_users: userLogin }
+          }
+        )
+
+        res.render("users/toBuy", {
+          title: "successful purchase",
+          styles: "carrito.css",
+          user: req.session.user,
+          newMembership
+        });
+
+      })
 
   },
-  queries: function (req,res){
+  queries: function (req, res) {
     db.ContactUs.findAll()
-    .then((queries) => {		
-      res.render("users/queries", {
-        title: "Queries List",
-        styles: "carrito.css",
-        user: req.session.user,
-        queries
-    });
-  })
+      .then((queries) => {
+        res.render("users/queries", {
+          title: "Queries List",
+          styles: "carrito.css",
+          user: req.session.user,
+          queries
+        });
+      })
   }
 
   // editaccount: function (req, res) {
